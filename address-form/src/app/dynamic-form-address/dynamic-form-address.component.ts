989bdd7fc2345e2f8646512310e8c574d174aebd
graff-ctrl <agraff@seattleu.edu>
2021-05-21 16:57:18 -0700

    Test branch.

--- address-form/src/app/dynamic-form-address/dynamic-form-address.component.ts
+++ address-form/src/app/dynamic-form-address/dynamic-form-address.component.ts
@@ -2,7 +2,7 @@ import { Component, Input } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 
 import { FieldBase } from '../shared/field-base';
-
+// This is a comment for the new branch.
 @Component({
   selector: 'app-field',
   templateUrl: './dynamic-form-address.component.html'
@@ -11,4 +11,5 @@ export class DynamicFormAddressComponent {
   @Input() field: FieldBase<string>;
   @Input() form: FormGroup;
   get isValid() { return this.form.controls[this.field.key].valid; }
+
 }
